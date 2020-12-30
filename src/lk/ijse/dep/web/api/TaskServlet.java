2efe904d3aa243b864530de1ea33be2fcdbb0225
Lucky Prabath <lucky.prabath94@gmail.com>
2020-12-30 23:51:34 +0530

    Edit delete, post, get, put methods in TaskServlet and complete

--- src/lk/ijse/dep/web/api/TaskServlet.java
+++ src/lk/ijse/dep/web/api/TaskServlet.java
@@ -3,6 +3,7 @@ package lk.ijse.dep.web.api;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import jakarta.json.bind.JsonbException;
+import lk.ijse.dep.web.model.Task;
 import lk.ijse.dep.web.model.User;
 import org.apache.commons.dbcp2.BasicDataSource;
 
@@ -30,25 +31,25 @@ public class TaskServlet extends HttpServlet {
 
         try {
             Jsonb jsonb = JsonbBuilder.create(); //entry point
-            User user = jsonb.fromJson(request.getReader(), User.class);
+            Task task = jsonb.fromJson(request.getReader(), Task.class);
             Connection connection = cp.getConnection();
             /*validate*/
-            if(user.getUser_id() == null || user.getUsername() == null || user.getPassword() == null || user.getName() == null){
+            if(task.getTask_id() == null || task.getTask_title() == null || task.getPriority() == null || task.getUser_id() == null){
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST);
                 return;
             }
 
-            if (!user.getUser_id().matches("U\\d{3}") || user.getUsername().trim().isEmpty() || user.getPassword().trim().isEmpty()
-                    || user.getName().trim().isEmpty()){
+            if (!task.getTask_id().matches("T\\d{3}") || task.getTask_title().trim().isEmpty() || task.getPriority().trim().isEmpty()
+                    || task.getUser_id().trim().isEmpty()){
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST);
                 return;
             }
 
-            PreparedStatement pstm = connection.prepareStatement("INSERT INTO User VALUES (?,?,?,?)");
-            pstm.setString(1, user.getUser_id());
-            pstm.setString(2, user.getUsername());
-            pstm.setString(3, user.getPassword());
-            pstm.setString(4, user.getName());
+            PreparedStatement pstm = connection.prepareStatement("INSERT INTO Task VALUES (?,?,?,?)");
+            pstm.setString(1, task.getTask_id());
+            pstm.setString(2, task.getTask_title());
+            pstm.setString(3, task.getPriority());
+            pstm.setString(4, task.getUser_id());
 
             if(pstm.executeUpdate()>0){
                 response.setStatus(HttpServletResponse.SC_CREATED);
@@ -68,30 +69,30 @@ public class TaskServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String id = request.getParameter("user_id");
+        String id = request.getParameter("task_id");
         BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
         response.setContentType("application/json");
         try (Connection connection = cp.getConnection()){
             PrintWriter out = response.getWriter();
-            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM User" + ((id != null) ? "WHERE id=?" : ""));
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Task" + ((id != null) ? "WHERE task_id=?" : ""));
             if (id != null){
                 pstm.setObject(1, id);
             }
             ResultSet rst = pstm.executeQuery();
-            List<User> userList = new ArrayList<>();
+            List<Task> taskList = new ArrayList<>();
             while (rst.next()){
                 id= rst.getString(1);
-                String username = rst.getString(2);
-                String password = rst.getString(3);
-                String name = rst.getString(4);
-                userList.add(new User(id, username, password, name));
+                String task_title = rst.getString(2);
+                String priority = rst.getString(3);
+                String user_id = rst.getString(4);
+                taskList.add(new Task(id, task_title, priority, user_id));
             }
 
-            if( id != null && userList.isEmpty()){
+            if( id != null && taskList.isEmpty()){
                 response.sendError(HttpServletResponse.SC_NOT_FOUND);
             } else {
                 Jsonb jsonb = JsonbBuilder.create();
-                out.println(jsonb.toJson(userList));
+                out.println(jsonb.toJson(taskList));
                 connection.close();
             }
         } catch (SQLException throwables) {
@@ -103,8 +104,8 @@ public class TaskServlet extends HttpServlet {
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String id = req.getParameter("user_id");
-        if (id == null || !id.matches("U\\d{3}")){
+        String id = req.getParameter("task_id");
+        if (id == null || !id.matches("T\\d{3}")){
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
@@ -112,25 +113,25 @@ public class TaskServlet extends HttpServlet {
         BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
         try (Connection connection = cp.getConnection()){
             Jsonb jsonb = JsonbBuilder.create();  //entry point
-            User user = jsonb.fromJson(req.getReader(), User.class);
+            Task task = jsonb.fromJson(req.getReader(), Task.class);
 
             /*validate*/
-            if( user.getUsername() == null || user.getPassword() == null || user.getName() == null){
+            if( task.getTask_title() == null || task.getPriority() == null || task.getUser_id() == null){
                 resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
                 return;
             }
 
-            if ( user.getUsername().trim().isEmpty() || user.getPassword().trim().isEmpty() || user.getName().trim().isEmpty()){
+            if ( task.getTask_title().trim().isEmpty() || task.getPriority().trim().isEmpty() || task.getUser_id().trim().isEmpty()){
                 resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
                 return;
             }
-            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM User WHERE user_id=?");
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Task WHERE task_id=?");
             pstm.setObject(1, id);
             if(pstm.executeQuery().next()) {
-                pstm = connection.prepareStatement("UPDATE User SET username=?, password=?, name=? WHERE user_id=?");
-                pstm.setObject(1, user.getUsername());
-                pstm.setObject(2, user.getPassword());
-                pstm.setObject(3, user.getName());
+                pstm = connection.prepareStatement("UPDATE Task SET task_title=?, priority=?, user_id=? WHERE task_id=?");
+                pstm.setObject(1, task.getTask_title());
+                pstm.setObject(2, task.getPriority());
+                pstm.setObject(3, task.getUser_id());
                 pstm.setObject(4, id);
                 if (pstm.executeUpdate() > 0) {
                     resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
@@ -149,18 +150,18 @@ public class TaskServlet extends HttpServlet {
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String id = req.getParameter("user_id");
-        if (id == null || !id.matches("U\\d{3}")){
+        String id = req.getParameter("task_id");
+        if (id == null || !id.matches("T\\d{3}")){
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
 
         BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
         try (Connection connection = cp.getConnection()){
-            PreparedStatement pstm = connection.prepareStatement("SELECT  * FROM User WHERE user_id=?");
+            PreparedStatement pstm = connection.prepareStatement("SELECT  * FROM Task WHERE task_id=?");
             pstm.setObject(1, id);
             if(pstm.executeQuery().next()) {
-                pstm = connection.prepareStatement("DELETE FROM User WHERE user_id=?");
+                pstm = connection.prepareStatement("DELETE FROM Task WHERE task_id=?");
                 pstm.setObject(1, id);
                 boolean success = pstm.executeUpdate() > 0;
                 if (success) {
