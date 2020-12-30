c57e70603f7dbcea47dd14cbc0bc66e06797da86
Lucky Prabath <lucky.prabath94@gmail.com>
2020-12-30 16:47:43 +0530

    Complete doGet mehod in UserServlet

--- src/lk/ijse/dep/web/api/UserServlet.java
+++ src/lk/ijse/dep/web/api/UserServlet.java
@@ -1,23 +1,75 @@
 package lk.ijse.dep.web.api;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import lk.ijse.dep.web.model.User;
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author : Lucky Prabath <lucky.prabath94@gmail.com>
  * @since : 2020-12-30
  **/
-@WebServlet(name = "UserServlet")
+@WebServlet(name = "UserServlet", urlPatterns = "/users")
 public class UserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("user_id");
+        BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
+        response.setContentType("application/json");
+        try (Connection connection = cp.getConnection()){
+            PrintWriter out = response.getWriter();
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM User" + ((id != null) ? "WHERE id=?" : ""));
+            if (id != null){
+                pstm.setObject(1, id);
+            }
+            ResultSet rst = pstm.executeQuery();
+            List<User> userList = new ArrayList<>();
+            while (rst.next()){
+                id= rst.getString(1);
+                String username = rst.getString(2);
+                String password = rst.getString(3);
+                String name = rst.getString(4);
+                userList.add(new User(id, username, password, name));
+            }
 
+            if( id != null && userList.isEmpty()){
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            } else {
+                Jsonb jsonb = JsonbBuilder.create();
+                out.println(jsonb.toJson(userList));
+                connection.close();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
     }
 }
